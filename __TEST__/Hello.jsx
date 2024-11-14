b4e72c654737cabcb304d6b8ce52447c3e12c9b0
cookingBird <d17381844023@gmail.com>
2024-11-15 01:27:25 +0800

    feat: support eslint@7

---
+++ __TEST__/Hello.jsx
@@ -0,0 +1,7 @@
+/* eslint-disable no-use-before-define */
+// eslint-disable-next-line  import/no-extraneous-dependencies
+import React from 'react';
+
+export default function HelloWorld() {
+  return <div>Hello World</div>;
+}
---
+++ __TEST__/Hello.tsx
@@ -0,0 +1,7 @@
+/* eslint-disable no-use-before-define */
+// eslint-disable-next-line  import/no-extraneous-dependencies
+import React from 'react';
+
+export default function HelloWorld(msg: string) {
+  return <div>Hello World {msg}</div>;
+}
